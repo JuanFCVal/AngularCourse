5ba216ea948008287437f545471142fbb4b0c555
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-09-19 17:37:38 -0500

    Ver pais detail page ready

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -9,10 +10,9 @@ import { switchMap } from 'rxjs/operators';
   ]
 })
 export class VerPaisComponent implements OnInit {
-
+  pais!: Country;
   constructor(private activatedRoute: ActivatedRoute,
     private paisService:PaisService) { }
-
   ngOnInit(): void {
     // this.activatedRoute.params.subscribe( params =>{
     //   console.log(params.codigoPais, "Id")
@@ -22,9 +22,11 @@ export class VerPaisComponent implements OnInit {
     // } )
 
     this.activatedRoute.params.pipe(  
-      switchMap((params) => this.paisService.getCountryById(params.codigoPais))
-    ).subscribe(resp =>{
-      console.log(resp)
+      switchMap((params) => this.paisService.getCountryById(params.codigoPais)),
+    
+    ).subscribe(pais =>{
+      this.pais = pais
+      console.log(pais)
     })
 
   }
