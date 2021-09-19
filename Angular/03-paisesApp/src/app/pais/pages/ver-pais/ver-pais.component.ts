15c22cb26c6d668df1e49465151772e31754e230
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-09-19 17:22:42 -0500

    Switchmap To return an observable from observable

--- Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -14,13 +14,18 @@ export class VerPaisComponent implements OnInit {
     private paisService:PaisService) { }
 
   ngOnInit(): void {
-    
-    this.activatedRoute.params.subscribe( params =>{
-      console.log(params.codigoPais, "Id")
-      this.paisService.getCountryById(params.codigoPais).subscribe( resp=>{
-        console.log(resp)
-      })
-    } )
+    // this.activatedRoute.params.subscribe( params =>{
+    //   console.log(params.codigoPais, "Id")
+    //   this.paisService.getCountryById(params.codigoPais).subscribe( resp=>{
+    //     console.log(resp)
+    //   })
+    // } )
+
+    this.activatedRoute.params.pipe(  
+      switchMap((params) => this.paisService.getCountryById(params.codigoPais))
+    ).subscribe(resp =>{
+      console.log(resp)
+    })
 
   }
 
