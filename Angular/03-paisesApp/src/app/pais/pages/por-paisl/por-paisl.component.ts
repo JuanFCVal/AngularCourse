3aef37cbf419f776248c0e9b22ba3026408eb6ba
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-09-19 22:27:34 -0500

    Add functionality to suggestion bar

--- Angular/03-paisesApp/src/app/pais/pages/por-paisl/por-paisl.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/por-paisl/por-paisl.component.ts
@@ -6,12 +6,19 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-paisl',
   templateUrl: './por-paisl.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer;
+    }
+    `
   ]
 })
 export class PorPaislComponent implements OnInit {
   termino: string  = ""
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
   constructor(private paisService : PaisService) { }
 
   ngOnInit(): void {
@@ -31,7 +38,16 @@ export class PorPaislComponent implements OnInit {
     )
   }
   sugerencias(termino: string){
+    this.termino = termino
     this.hayError = false;
-
+    this.mostrarSugerencias = true;
+this.paisService.buscarPais(termino).subscribe(paises =>{
+  this.paisesSugeridos = paises
+  this.paisesSugeridos = paises.splice(0,5),
+  (err) =>this.paisesSugeridos = []
+})
+  }
+  buscarSugerido(termino : string){
+    this.buscar(termino);
   }
 }
