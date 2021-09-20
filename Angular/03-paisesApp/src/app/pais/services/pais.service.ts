d41ad5fa52f18da101f22e90132c819fc2fb8e04
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2021-09-19 22:07:17 -0500

    Optimizate Http Request with parameters

--- Angular/03-paisesApp/src/app/pais/services/pais.service.ts
+++ Angular/03-paisesApp/src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -9,21 +9,27 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
   
   private _apiUrl: string = "https://restcountries.eu/rest/v2"
+  get httpParams(){
+    return new HttpParams().set(
+      'fields', 'name;capital;alpha2code;flag;population'
+    )}
   constructor(private http: HttpClient) { }
   buscarPais(termino: string): Observable<Country[]>{
     const url = `${this._apiUrl}/name/${termino}`
-    return this.http.get<Country[]>(url); //Se agrega el tipado de la respuesta
+    return this.http.get<Country[]>(url, {params : this.httpParams}); //Se agrega el tipado de la respuesta
   }
   buscarCapital(termino: string): Observable<Country[]>{
     const url = `${this._apiUrl}/capital/${termino}`
-    return this.http.get<Country[]>(url); //Se agrega el tipado de la respuesta
+    return this.http.get<Country[]>(url, {params : this.httpParams}); //Se agrega el tipado de la respuesta
   }
   getCountryById(termino: string): Observable<Country>{
     const url = `${this._apiUrl}/alpha/${termino}`
     return this.http.get<Country>(url); //Se agrega el tipado de la respuesta
   }
-  getCountryByRegion(termino: string): Observable<Country[]>{
+  getCountryByRegion(termino: string): Observable<Country[]>{{
+
     const url = `${this._apiUrl}/region/${termino}`
-    return this.http.get<Country[]>(url); //Se agrega el tipado de la respuesta
+    return this.http.get<Country[]>(url, {params : this.httpParams}); //Se agrega el tipado de la respuesta
+  }
   }
 }
